b363cf09ebb4dd4f8b6241961175f2218dd2e78f
Nikita Koliadin <qThegamEp@gmail.com>
2019-07-27 11:24:37 +0300

    Updates

---
+++ precache-manifest.9c739ddff9fda1ff9b7e369e258f2464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45cf15aa542a4c16fb764b12df28a2",
+    "url": "/file.stream.app/index.html"
+  },
+  {
+    "revision": "9fd4eb26c8e20fe9d6cd",
+    "url": "/file.stream.app/static/css/main.e2473e6b.chunk.css"
+  },
+  {
+    "revision": "9255a286d3e388a1bb2a",
+    "url": "/file.stream.app/static/js/2.ecace2bc.chunk.js"
+  },
+  {
+    "revision": "9fd4eb26c8e20fe9d6cd",
+    "url": "/file.stream.app/static/js/main.e751d1eb.chunk.js"
+  },
+  {
+    "revision": "b3ff94a4b814c50a98c9",
+    "url": "/file.stream.app/static/js/runtime~main.d7fd9b1f.js"
+  }
+]);
