59c6bc67aa835a9c07d3ad360040ed706c68506e
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-01 11:10:53 +0300

    Updated reducer.js: - Added preloaderReducer as preloader;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import preloaderReducer from './preloaderReducer';
+
 const reducers = combineReducers({
     routing: routerReducer,
+    preloader: preloaderReducer,
 });
 
 export default reducers;
