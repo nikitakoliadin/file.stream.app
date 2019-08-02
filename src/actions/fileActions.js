80d97620db3c3999ee64a6d1560c3917ab580ed3
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-02 13:29:39 +0300

    Updated fileActions.js: - Implemented reading files using FileStream;

--- src/actions/fileActions.js
+++ src/actions/fileActions.js
@@ -1,11 +1,42 @@
 import * as types from '../constants/actionTypes';
 
 import {startPreloader, finishPreloader} from './preloaderActions';
+import {FileStream, readTypes} from '../js/FileStream';
+import * as appConstants from '../constants/appConstants';
 
 export const onDropFiles = (acceptedFiles, rejectedFiles) => dispatch => {
     dispatch(startPreloader());
     dispatch(startFilesProcessing({acceptedFiles, rejectedFiles}));
     try {
+        acceptedFiles.forEach((file, index) => {
+            if (index !== acceptedFiles.length - 1) {
+                const fileStream = new FileStream(appConstants.FILE_STREAM_CHUNK_SIZE);
+                fileStream.read(
+                    file,
+                    readTypes.AS_TEXT,
+                    file => {
+                        console.log("File is loaded:", file);
+                    },
+                    (file, event) => {
+                        console.log("File is loading:", file.name, "Loaded chunk size:", event.loaded, "bytes");
+                    }
+                );
+            } else {
+                const fileStream = new FileStream(appConstants.FILE_STREAM_CHUNK_SIZE);
+                fileStream.read(
+                    file,
+                    readTypes.AS_TEXT,
+                    file => {
+                        console.log("File is loaded:", file);
+                        dispatch(finishFilesProcessing());
+                        dispatch(finishPreloader());
+                    },
+                    (file, event) => {
+                        console.log("File is loading:", file.name, "Loaded chunk size:", event.loaded, "bytes");
+                    }
+                );
+            }
+        });
     } catch (e) {
         dispatch(finishFilesProcessing());
         dispatch(filesProcessingError(e.message));
