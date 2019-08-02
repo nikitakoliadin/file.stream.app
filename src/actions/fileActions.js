8562e3542e6a9afef3dcbc63ed8fc5aebde5a2fd
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-02 12:00:26 +0300

    Updated fileActions.js: - Added filesProcessingError action; - Updated startFilesProcessing action; - Refactored onDropFiles action;

--- src/actions/fileActions.js
+++ src/actions/fileActions.js
@@ -4,21 +4,31 @@ import {startPreloader, finishPreloader} from './preloaderActions';
 
 export const onDropFiles = (acceptedFiles, rejectedFiles) => dispatch => {
     dispatch(startPreloader());
-    dispatch(startFilesProcessing());
-    console.log("Accepted files:", acceptedFiles);
-    console.log("Rejected files:", rejectedFiles);
-    dispatch(finishFilesProcessing());
-    dispatch(finishPreloader());
+    dispatch(startFilesProcessing({acceptedFiles, rejectedFiles}));
+    try {
+    } catch (e) {
+        dispatch(finishFilesProcessing());
+        dispatch(filesProcessingError(e.message));
+        dispatch(finishPreloader());
+    }
 };
 
-const startFilesProcessing = () => {
+const startFilesProcessing = files => {
     return {
-        type: types.START_FILES_PROCESSING_TYPE
+        type: types.START_FILES_PROCESSING_TYPE,
+        payload: files
     }
 };
 
 const finishFilesProcessing = () => {
     return {
         type: types.FINISH_FILES_PROCESSING_TYPE
+    }
+};
+
+const filesProcessingError = message => {
+    return {
+        type: types.FILES_PROCESSING_ERROR_TYPE,
+        payload: message
     }
 };
