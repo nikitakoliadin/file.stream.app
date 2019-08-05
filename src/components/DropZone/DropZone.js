6ae1f1f52b7fba06a8f6d5630053ce81da21bbfb
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-05 08:47:17 +0300

    - Created DropZone component; - Changed DropZone to dumb component;

---
+++ src/components/DropZone/DropZone.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dropzone from 'react-dropzone';
+
+import styles from './DropZone.module.css';
+
+class DropZone extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.handleOnDrop = this.handleOnDrop.bind(this);
+    }
+
+    handleOnDrop(acceptedFiles, rejectedFiles) {
+        const {onDropFiles} = this.props;
+        onDropFiles(acceptedFiles, rejectedFiles);
+    }
+
+    render() {
+        return (
+            <div id={"DropZone"}
+                 className={styles.DropZone}>
+                <Dropzone onDrop={this.handleOnDrop}
+                          className={styles.Dropzone}>
+                    Drop file's here
+                </Dropzone>
+            </div>
+        );
+    }
+}
+
+DropZone.propTypes = {
+    onDropFiles: PropTypes.func.isRequired
+};
+
+export default DropZone;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Dropzone from 'react-dropzone';
 
+import DropZone from '../DropZone/DropZone';
 import styles from './Main.module.css';
 
-class Main extends Component {
-
-    constructor(props, context) {
-        super(props, context);
-        this.handleOnDrop = this.handleOnDrop.bind(this);
-    }
-
-    handleOnDrop(acceptedFiles, rejectedFiles) {
-        const {onDropFiles} = this.props;
-        onDropFiles(acceptedFiles, rejectedFiles);
-    }
-
-    render() {
-        return (
-            <div id={"Main"}
-                 className={styles.Main}>
-                <Dropzone onDrop={this.handleOnDrop}
-                          className={styles.Dropzone}>
-                    Drop file's here
-                </Dropzone>
-            </div>
-        );
-    }
-}
+const Main = props => {
+    const {onDropFiles} = props;
+    return (
+        <div id={"Main"}
+             className={styles.Main}>
+            <DropZone onDropFiles={onDropFiles}/>
+        </div>
+    );
+};
 
 Main.propTypes = {
     onDropFiles: PropTypes.func.isRequired
