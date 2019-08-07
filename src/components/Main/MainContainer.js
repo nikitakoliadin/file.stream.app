4b63c88051be0941d0a345e0c5410e739dee2bcd
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-07 14:00:39 +0300

    Updated MainContainer.js: - Added chunkPicker state; - Added setChunkSize action; - Updated getMainComponent method;

--- src/components/Main/MainContainer.js
+++ src/components/Main/MainContainer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+import {setChunkSize} from '../../actions/chunkPickerAction';
 import {onDropFiles} from '../../actions/fileActions';
 import Main from './Main';
 import Preloader from '../Preloader/Preloader';
@@ -11,8 +12,38 @@ import * as colors from '../../constants/colors';
 class MainContainer extends Component {
 
     getMainComponent() {
-        const {onDropFiles} = this.props;
-        return <Main onDropFiles={onDropFiles}/>
+        const {chunkPicker} = this.props;
+        const {setChunkSize, onDropFiles} = this.props;
+        const chunkSizeValues = [
+            {
+                key: "8 B",
+                value: appConstants.EIGHT_BYTES
+            },
+            {
+                key: "1 KB",
+                value: appConstants.ONE_KILOBYTE
+            },
+            {
+                key: "1 MB",
+                value: appConstants.ONE_MEGABYTE
+            },
+            {
+                key: "10 MB",
+                value: appConstants.TEN_MEGABYTES
+            },
+            {
+                key: "100 MB",
+                value: appConstants.ONE_HUNDRED_MEGABYTES
+            },
+            {
+                key: "1 GB",
+                value: appConstants.ONE_GIGABYTE
+            }
+        ];
+        return <Main chunkPicker={chunkPicker}
+                     chunkSizeValues={chunkSizeValues}
+                     setChunkSize={setChunkSize}
+                     onDropFiles={onDropFiles}/>
     }
 
     getPreloaderComponent() {
@@ -35,12 +66,14 @@ class MainContainer extends Component {
 
 function mapStateToProps(state) {
     return {
+        chunkPicker: state.chunkPicker,
         preloader: state.preloader
     };
 }
 
 function matchDispatchToProps(dispatch) {
     return bindActionCreators({
+        setChunkSize,
         onDropFiles
     }, dispatch);
 }
