7a3fba3d03bd4095459f14e3b50199b3cf22f5a1
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-07 13:33:11 +0300

    Updated reducer.js: - Added chunkPickerReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import chunkPickerReducer from './chunkPickerReducer';
 import preloaderReducer from './preloaderReducer';
 
 const reducers = combineReducers({
     routing: routerReducer,
+    chunkPicker: chunkPickerReducer,
     preloader: preloaderReducer,
 });
 
