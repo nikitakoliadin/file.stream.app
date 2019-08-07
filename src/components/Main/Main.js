ae8d4b8f494089390a52b86c9e5ed8b7a765d034
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-07 14:05:15 +0300

    Updated Main.js: - Added ChunkPicker component; - Added chunkPicker state; - Added setChunkSize action; - Updated getMainComponent method;

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ChunkPicker from '../ChunkPicker/ChunkPicker';
 import DropZone from '../DropZone/DropZone';
 import styles from './Main.module.css';
 
 const Main = props => {
-    const {onDropFiles} = props;
+    const {chunkPicker, chunkSizeValues} = props;
+    const {setChunkSize, onDropFiles} = props;
     return (
         <div id={"Main"}
              className={styles.Main}>
+            <ChunkPicker chunkPicker={chunkPicker}
+                         chunkSizeValues={chunkSizeValues}
+                         setChunkSize={setChunkSize}/>
             <DropZone onDropFiles={onDropFiles}/>
         </div>
     );
 };
 
 Main.propTypes = {
+    chunkPicker: PropTypes.object.isRequired,
+    chunkSizeValues: PropTypes.array.isRequired,
+    setChunkSize: PropTypes.func.isRequired,
     onDropFiles: PropTypes.func.isRequired
 };
 
